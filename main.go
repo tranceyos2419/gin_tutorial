bf4a7eb7993cfb6c894807826e5b423a4c178184
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-10-23 16:55:59 +0900

    build: running the server

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	server := gin.Default()
+
+	server.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "gin",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	server.Run(":8000")
 }
